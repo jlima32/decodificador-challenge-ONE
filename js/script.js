d64932f896fb0660c513d6a6353c7fd6cd93c3f3
Juan Lima <juangustavo@gmail.com>
2024-01-25 12:48:27 -0300

    feat: function descriptografar

--- js/script.js
+++ js/script.js
@@ -21,6 +21,26 @@ function criptografar(){
         </div>
         `
     }
+}
 
+
+function descriptografar(){
+    if (verificaText() == ''){
+        alert('digite um texto para criptografar ou descriptografar')
+    }else{
+        let string = document.getElementById('texto').value;
+        let stringE = string.replaceAll("enter", "e");
+        let stringI = stringE.replaceAll("imes", "i");
+        let stringA = stringI.replaceAll("ai", "a");
+        let stringO = stringA.replaceAll("ober", "o");
+        let stringU = stringO.replaceAll("ufat", "u");
+        
+        document.querySelector('.result').innerHTML =`
+        <div class="textResult">
+        <p class="text">${stringU}</p>
+        <button type="button" class="btnCopiar">Copiar Texto</button>
+        </div>
+        `
+    }
 }
 
